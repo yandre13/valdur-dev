bdd7c5bfa2690394bc04c9bfa33a43e34ffa28cc
yandre13 <yandre13.elsf@gmail.com>
2023-03-21 08:26:28 -0500

    feat: Experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import up from "../assets/svg/up.svg";
+
+function Item() {
+
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => {
+    setOpen(!open)
+    console.log({ open })
+  }
+  return (
+    <li className="experience-item">
+
+      <div className="experience-date">
+        <h3>
+          Nov 2019 - Present
+        </h3>
+      </div>
+
+      <div className="experience-container">
+        <h4>
+          Product Designer
+        </h4>
+        <div className={`experience-content ${open ? 'active' : ''}`}>
+          <h4>
+            Encora Inc.
+          </h4>
+          <ul>
+            <li>
+              Researching and identifying user needs: Conduct research to understand user needs, preferences, and pain points. Develop products that meet the needs of their target audience.
+            </li>
+            <li>
+              Conceptualizing and prototyping: Create sketches, mockups, and prototypes to visualize and test their ideas. They iterate on their designs based on user feedback and usability testing.
+            </li>
+            <li>
+              Creating detailed product designs: Create detailed drawings and specifications for their designs. They work closely with engineers and other stakeholders to ensure their designs can be produced efficiently and cost-effectively.
+            </li>
+            <li>
+              Collaborating with cross-functional teams: Work with a variety of stakeholders, including engineers, marketers, and project managers, to ensure the successful launch of a product.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="button" role="button" onClick={handleClick}>
+        <img src={up} alt="Arrow" />
+      </div>
+
+
+
+    </li>
+  )
+}
+
+export default function Experience() {
+  return (
+    <section className="Experience container">
+      <h3>
+        Experience:
+      </h3>
+
+      <ul className="experience-list">
+        {
+          Array(3).fill().map((_, i) => <Item key={i} />)
+        }
+      </ul>
+    </section>
+  )
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,23 +7,43 @@ import letter from "../assets/svg/letter.svg";
 import play from "../assets/svg/play.svg";
 
 import Circles from "../components/Circles";
+import LetsTalk from "../components/LetsTalk";
+import Experience from "../components/Experience";
 
 //icons
 
+// useEffect(() => {
+//   const dotsSequence = [".", "..", "...", ".."];
+//   let index = 0;
+//   let direction = 1;
+
+//   const intervalId = setInterval(() => {
+//     setDots(dotsSequence[index]);
+//     index += direction;
+//     if (index === dotsSequence.length - 1 || index === 0) {
+//       direction = -direction;
+//     }
+//   }, 500);
+
+//   return () => clearInterval(intervalId);
+// }, []);
+
 export default function About() {
+
+
   return (
-    <main>
+    <main className="AboutPage">
 
       {/* <Balls /> */}
-      <section className="AboutPage About container">
+      <section className="About container">
         <div className="image">
           <img className="photo" src="/img/AboutPhoto.png" alt="About me" />
         </div>
 
         <div className="about-text">
-          <h1 className="title-section">
+          <h2 className="title-section">
             About me<span className="text-orange">.</span>
-          </h1>
+          </h2>
 
           <p className="description-section text-justify">
             With more than 5 years of experience, with a strong background in digital products. I am dedicated to working closely with clients to understand their needs and preferences, and I am always open to feedback to ensure that the end result is a product that exceeds expectations. Whether it's a website, mobile app, or physical product, I am confident in my ability to create designs that make a lasting impact.
@@ -37,26 +57,11 @@ export default function About() {
         </div>
       </section>
 
+      <Experience />
 
-      <section className="LetsTalk container">
+      <div className="spacer"></div>
 
-        <div className="lets-talk-text">
-          <h3 className="title-section">
-            <span className="font-rainbow text-trans-initial">Let’s talk</span> about the next big thing
-          </h3>
-
-          <p className="description-section">
-            If you have other requests or questions about projects, don’t hesitate to email me at <span className="font-bold">hi@valdurdesign.com</span> or send me a message.
-          </p>
-
-          <div className="button-container">
-            <button className="main-button">Send a message</button>
-          </div>
-        </div>
-
-        <Visit />
-
-      </section>
+      <LetsTalk />
     </main>
   );
 }
